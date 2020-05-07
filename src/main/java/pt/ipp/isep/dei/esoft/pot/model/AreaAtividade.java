5ed12e3ee68149aa16e580d78d8bfc162c66f3b1
vfraanco <57173400+vfraanco@users.noreply.github.com>
2020-05-07 21:51:55 +0100

    Update AreaAtividade.java

--- src/main/java/pt/ipp/isep/dei/esoft/pot/model/AreaAtividade.java
+++ src/main/java/pt/ipp/isep/dei/esoft/pot/model/AreaAtividade.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pt.ipp.isep.dei.esoft.pot.model;
 
 import java.util.Objects;
 
-/**
- *
- * @author paulomaio
- */
+
 public class AreaAtividade
 {
     private String m_strCodigo;
